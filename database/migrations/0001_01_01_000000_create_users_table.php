b98c382f7e762f116b922dca791b1271b5e46d2e
Guilherme Silva <guiobmep@gmail.com>
2025-05-22 23:54:12 -0300

    criando models e migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('cardo_do_sistema');
+            $table->string('cpf');
+            $table->string('cargo');
+            $table->string('telefone');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
