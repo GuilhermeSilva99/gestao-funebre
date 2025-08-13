1a4468974f87ff5adcd48eb9094d629d5c617363
Guilherme Silva <guiobmep@gmail.com>
2025-08-13 00:38:28 -0300

    Cria model, controller e migration de midia

---
+++ app/Models/Midia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Midia extends Model
+{
+    protected $fillable = [
+        'midia',
+        'defunto_id'
+    ];
+}
